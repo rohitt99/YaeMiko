a2e9aa930ee1a6d493a5194988734bda6786c341
rohitt99 <164199173+rohitt99@users.noreply.github.com>
2024-05-22 21:40:24 +0530

    Update karma.py

--- Infamous/karma.py
+++ Infamous/karma.py
@@ -10,31 +10,31 @@ from Mikobot import BOT_USERNAME, OWNER_ID, SUPPORT_CHAT
 
 # <============================================== CONSTANTS =========================================================>
 START_IMG = [
-    "https://telegra.ph/file/40b93b46642124605e678.jpg",
-    "https://telegra.ph/file/01a2e0cd1b9d03808c546.jpg",
-    "https://telegra.ph/file/ed4385c26dcf6de70543f.jpg",
-    "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg",
-    "https://telegra.ph/file/cce9038f6a9b88eb409b5.jpg",
-    "https://telegra.ph/file/262c86393730a609cdade.jpg",
-    "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg",
+    "https://graph.org/file/7c31b0b6bcaea61e5e236.jpg",
+    "https://graph.org/file/daf0e8859c1922ea414e4.png",
+    "https://graph.org/file/53cecd6ddac2438fa44fc.jpg",
+    "https://graph.org/file/0f9d099b45074cb9a2ed3.png",
+    "https://graph.org/file/8c68edda7a29ef01a92b8.jpg",
+    "https://graph.org/file/01937fbb3f7c60104f60a.jpg",
+    "https://graph.org/file/c3f9e65149f568b9891bb.jpg",
 ]
 
-HEY_IMG = "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg"
+HEY_IMG = "https://graph.org/file/53cecd6ddac2438fa44fc.jpg"
 
 ALIVE_ANIMATION = [
-    "https://telegra.ph//file/f9e2b9cdd9324fc39970a.mp4",
-    "https://telegra.ph//file/8d4d7d06efebe2f8becd0.mp4",
-    "https://telegra.ph//file/c4c2759c5fc04cefd207a.mp4",
-    "https://telegra.ph//file/b1fa6609b1c4807255927.mp4",
-    "https://telegra.ph//file/f3c7147da6511fbe27c25.mp4",
-    "https://telegra.ph//file/39071b73c02e3ff5945ca.mp4",
-    "https://telegra.ph//file/8d4d7d06efebe2f8becd0.mp4",
-    "https://telegra.ph//file/6efdd8e28756bc2f6e53e.mp4",
+    "https://graph.org/file/acdef9bdb57502ce46ac3.mp4",
+    "https://graph.org/file/ae86c56f2183f8fada1c0.mp4",
+    "https://graph.org/file/acf9ed31939429215a40a.mp4",
+    "https://graph.org/file/1ceadb2464668f17dac50.mp4",
+    "https://graph.org/file/669efe1844cadc164c876.mp4",
+    "https://graph.org/file/b13a4e69d74f37fbce5e5.mp4",
+    "https://graph.org/file/51e40386b28d283940428.mp4",
+    "https://graph.org/file/acdef9bdb57502ce46ac3.mp4",
 ]
 
 FIRST_PART_TEXT = "✨ *ʜᴇʟʟᴏ* `{}` . . ."
 
-PM_START_TEXT = "✨ *ɪ ᴀᴍ ᴍɪᴋᴏ, ᴀ ɢᴇɴꜱʜɪɴ ɪᴍᴘᴀᴄᴛ ᴛʜᴇᴍᴇᴅ ʀᴏʙᴏᴛ ᴡʜɪᴄʜ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴍᴀɴᴀɢᴇ ᴀɴᴅ ꜱᴇᴄᴜʀᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ʜᴜɢᴇ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ*"
+PM_START_TEXT = "✨ *ɪ ᴀᴍ 𝑰𝒎𝒑𝒆𝒓𝒊𝒍𝒎𝒆𝒏𝒕, ᴀ ɢᴇɴꜱʜɪɴ ɪᴍᴘᴀᴄᴛ ᴛʜᴇᴍᴇᴅ ʀᴏʙᴏᴛ ᴡʜɪᴄʜ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴍᴀɴᴀɢᴇ ᴀɴᴅ ꜱᴇᴄᴜʀᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ʜᴜɢᴇ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ*"
 
 START_BTN = [
     [
@@ -71,8 +71,8 @@ GROUP_START_BTN = [
 
 ALIVE_BTN = [
     [
-        ib(text="UPDATES", url="https://t.me/Hydra_Updates"),
-        ib(text="SUPPORT", url="https://t.me/hydraXsupport"),
+        ib(text="UPDATES", url="https://t.me/IMPERILMENT_SUPPORT"),
+        ib(text="SUPPORT", url="https://t.me/IMPERILMENT_SUPPORT"),
     ],
     [
         ib(
@@ -83,7 +83,7 @@ ALIVE_BTN = [
 ]
 
 HELP_STRINGS = """
-🫧 *Yae-Miko* 🫧
+🫧 *𝑰𝒎𝒑𝒆𝒓𝒊𝒍𝒎𝒆𝒏𝒕* 🫧
 
 ☉ *Here, you will find a list of all the available commands.*
 
